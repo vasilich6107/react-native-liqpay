52849f5fec2cd8ab533d4763a5dc35a7daaa0416
vasilich <v.ditsyak@gmail.com>
2018-03-07 16:02:18 +0200

    Code cleanup

--- LiqpayCheckout.js
+++ LiqpayCheckout.js
@@ -2,11 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { requireNativeComponent } from 'react-native';
 
-RNLiqpayCheckout = requireNativeComponent('RNLiqpayCheckout', null);
+const RNLiqpayCheckout = requireNativeComponent('RNLiqpayCheckout', null);
 
-LiqpayCheckout = props => <RNLiqpayCheckout {...props}/>;
+const LiqpayCheckout = props => <RNLiqpayCheckout {...props} />;
 
 LiqpayCheckout.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
   params: PropTypes.object.isRequired,
   privateKey: PropTypes.string.isRequired,
   onLiqpaySuccess: PropTypes.func.isRequired,
